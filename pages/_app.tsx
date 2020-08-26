c4e050ad80e7c63d710f340a83abd212d7e1cd19
Naoto Kaneko <naoty.k@gmail.com>
2020-08-26 18:37:03 +0900

    Set up ESLint

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import "normalize.css"
 import { AppProps } from "next/app"
+import React, { ReactElement } from "react"
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps }: AppProps): ReactElement {
   return <Component {...pageProps} />
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,5 @@
-export default function Index() {
+import React, { ReactElement } from "react";
+
+export default function Index(): ReactElement {
   return <h1>Welcome to Next.js!</h1>
 }
